d2bbe98ed350db9dc838b96a23c3b7639f1b5957
Mohamed Miled <mohamed.miled@tuhh.de>
2025-06-15 01:46:54 +0200

    project1 first commit

--- project1/urls.py
+++ project1/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
 
-app_name = 'project1'
+app_name = "project1"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path('data/', views.data_upload, name='data_upload'),
+    
 ]
